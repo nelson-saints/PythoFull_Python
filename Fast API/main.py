c84ca068a4fe1006198d0e13f36cdc5a02f4b712
Nelson Silva <nelsonsilva25contato@gmail.com>
2024-10-09 17:44:16 -0300

    finalização
    Finalizado o projeto de cadastro e login com banco de dados via API

--- Fast API/main.py
+++ Fast API/main.py
@@ -1,34 +1,47 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from fastapi import FastAPI
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import datetime
-import pymysql
+from model import CONN, Pessoa, Tokens
+from secrets import token_hex
 
-USUARIO = 'root'
-SENHA = 'silva'
-HOST = 'localhost'
-BANCO = 'teste_api'
-PORT = '3306'
+app = FastAPI()
 
-CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
+def conectaBanco():
+    engine = create_engine(CONN, echo=True)
+    Session = sessionmaker(bind=engine)
+    return Session()
 
-engine = create_engine(CONN, echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
-
-class Pessoa(Base):
-    __tablename__ = 'Pessoa'
-    id = Column(Integer, primary_key=True)
-    nome = Column(String(50))
-    usuario = Column(String(20))
-    senha = Column(String(10))
+@app.post('/cadastro')
+def cadastro(nome: str, user: str, senha: str):
+    session = conectaBanco()
+    usuario = session.query(Pessoa).filter_by(usuario = user, senha=senha).all()
+    if len(usuario) ==0:
+        x = Pessoa(nome=nome, usuario=user, senha=senha)
+        session.add(x)
+        session.commit()
+        return{'status': 'sucesso'}
+    elif len(usuario) > 0:
+        return {'status': 'Usuario já cadastrado'}
     
-class Tokens(Base):
-    __tablename__ = 'Tokens'
-    id = Column(Integer, primary_key=True)
-    id_pessoa = Column(Integer, ForeignKey('Pessoa.id'))
-    token = Column(String(100))
-    data = Column(DateTime, default=datetime.datetime.utcnow())
+@app.post('/login')
+def login(usuario: str, senha: str):
+    session = conectaBanco()
+    user = session.query(Pessoa).filter_by(usuario=usuario, senha=senha).all()
+    if len(user) == 0:
+        return {'status': 'usuario inexistente'}
     
-Base.metadata.create_all(engine)
+    while True:
+        token = token_hex(50)
+        tokenExiste = session.query(Tokens).filter_by(token=token).all()
+        if len(tokenExiste) ==0:
+            pessoaExiste = session.query(Tokens).filter_by(id_pessoa=user[0].id).all()
+            if len(pessoaExiste) ==0:
+                novoToken = Tokens(id_pessoa=user[0].id, token=token)
+                session.add(novoToken)
+            elif len(pessoaExiste) > 0:
+                pessoaExiste[0].token = token
+                
+            session.commit()
+            break
+    return token
